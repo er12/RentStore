0e7b17786aee15ba49019518dac4e174b7c33ea6
er12 <ernestorod27@gmail.com>
2016-11-23 00:31:44 -0400

    Initial commit from start.spring.io

---
+++ src/main/java/edu/pucmm/pwa/RentStoreApplication.java
@@ -0,0 +1,12 @@
+package edu.pucmm.pwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentStoreApplication.class, args);
+	}
+}
